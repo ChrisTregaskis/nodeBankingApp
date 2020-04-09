c0a711295ec3eb5f6b4273a680385fecdc103329
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-09 10:25:37 +0100

    required in express to index.js added port and app.listen for nodemon

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const url = 'mongodb://localhost:27017';
+
+
+
+
+app.listen(port, () => console.log(`nodeBankingApp listening at http://localhost:${port}`));
