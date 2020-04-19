bc806c712343cc989bc3e6908c42447960df5adf
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-19 13:52:57 +0100

    imported customer accounts table component and included in render

--- bankingAppFE/banking-app/src/App.js
+++ bankingAppFE/banking-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import AddAccountForm from "./Components/AddAccountForm/AddAccountForm";
+import CustomerAccountsTable from "./Components/CustomerAccountsTable/CustomerAccountsTable";
 
 function App() {
   return (
     <div className="App container">
       <h1>Admin Page.</h1>
         <AddAccountForm/>
+        <CustomerAccountsTable/>
     </div>
   );
 }
