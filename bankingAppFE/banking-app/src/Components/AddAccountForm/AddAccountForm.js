e8c7f8f4cd1de0ada7c2081217058d78bf82f0d9
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-19 10:05:54 +0100

    created constructor and set state, methods to handle change and submit, generic send request function to move up-to parent component, amended render form

--- bankingAppFE/banking-app/src/Components/AddAccountForm/AddAccountForm.js
+++ bankingAppFE/banking-app/src/Components/AddAccountForm/AddAccountForm.js
@@ -2,19 +2,71 @@ import React from "react";
 import './addAccountForm.css';
 
 class AddAccountForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: '',
+            surname: '',
+            balance: 'Enter balance...'
+        };
+    }
+
+    handleChangeFirstName = (event) => {
+        this.setState({firstName: event.target.value});
+    };
+
+    handleChangeSurname = (event) => {
+        this.setState({surname: event.target.value});
+    };
+
+    handleChangeBalance = (event) => {
+        this.setState({balance: event.target.value})
+    };
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        let data = {
+            "customer_fname": this.state.firstName,
+            "customer_sname": this.state.surname,
+            "balance": parseInt(this.state.balance)
+        };
+
+        console.log(data)
+
+        await this.sendRequest('http://localhost:8080/customerAccounts', 'POST', data)
+    };
+
+
+    sendRequest = async (url, requestMethod, data) => {
+        let requestData = JSON.stringify(data);
+
+        let response = await fetch(url, {
+            method: requestMethod.toUpperCase(),
+            body: requestData,
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        });
+
+        let responseData = await response.json();
+        //use set state in order to rerender a box that holds the success/failure message
+        console.log(responseData);
+
+    };
+
+
     render() {
         return (
             <div className="addAccountContainer">
-                <form method="post" name="addAccountForm">
-                    <input type="text" name="firstName" id="input-first-name" placeholder="First name..." 
-                           className="col-md-4 formInputs"/>
-                    <input type="text" name="surname" id="input-surname" placeholder="Surname..."
-                           className="col-md-4 formInputs"/>
-                    <input type="number" name="balance" id="input-balance" placeholder="Balance..."
-                           className="col-md-4 formInputs"/>
-                    <button type="submit" value="submit" id="addAccount-submit" 
-                            className="btn btn-success">Add Account
-                    </button>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.firstName} onChange={this.handleChangeFirstName} placeholder="First name..."
+                           className="col-md-4 formInputs" required/>
+                    <input type="text" value={this.state.surname} onChange={this.handleChangeSurname} placeholder="Surname..."
+                           className="col-md-4 formInputs" required/>
+                    <input type="number" value={this.state.balance} onChange={this.handleChangeBalance} placeholder="Balance..."
+                           className="col-md-4 formInputs" required/>
+                    <input type="submit" value="Add Account" className="btn btn-success"/>
                 </form>
             </div>
         );
