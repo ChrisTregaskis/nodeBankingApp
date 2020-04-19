049b7cc235290550ec1df45e2d526260ab8ccff9
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-19 13:53:48 +0100

    set up component basic structure with js file and imported relevant style sheet and React

---
+++ bankingAppFE/banking-app/src/Components/CustomerAccountsTable/CustomerAccountsTable.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './customerAccountsTable.css';
+
+class CustomerAccountsTable extends React.Component {
+    
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CustomerAccountsTable;
