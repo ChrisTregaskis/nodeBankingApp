7cd88c1c796501d6dfc4927a0008c0c34d26bcac
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-26 11:34:59 +0100

    successfully got all accounts viewing on the page

--- bankingAppFE/banking-app/src/Components/CustomerAccountsTable/CustomerAccountsTable.js
+++ bankingAppFE/banking-app/src/Components/CustomerAccountsTable/CustomerAccountsTable.js
@@ -1,16 +1,36 @@
 import React from "react";
 import './customerAccountsTable.css';
-import customerData from './customerData';
-const columnHeader = ['First-name', 'Surname', 'Balance', 'Actions...', '', '', ''];
+const columnHeader = ['First-name', 'Surname', 'Balance', 'Deposit', 'Withdraw', 'Delete'];
 
 class CustomerAccountsTable extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            customerAccPackage: {
+                "customerAccounts": []
+            }
         }
     }
+
+    componentDidMount = () => {
+        this.fetchCustomerAccounts();
+    };
+
+    fetchCustomerAccounts = () => {
+        fetch('http://localhost:8080/customerAccounts', {
+            method: 'GET',
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        })
+        .then(data=>data.json())
+        .then((data)=>{
+            this.setState({
+                customerAccPackage: data
+            });
+        })
+    };
 
     generateHeader = () => {
         let result = [];
@@ -22,29 +42,32 @@ class CustomerAccountsTable extends React.Component {
 
     generateTableData = () => {
         let result = [];
-        let tableData = customerData.customerAccounts;
+        let tableData = this.state.customerAccPackage.customerAccounts;
 
         for(var i = 0; i < tableData.length; i++) {
             result.push(
                 <tr key={i} data-id={tableData[i]._id}>
                     <td key={tableData[i].customer_fname}>{tableData[i].customer_fname}</td>
                     <td key={tableData[i].customer_sname}>{tableData[i].customer_sname}</td>
-                    <td key={tableData[i].balance}>{tableData[i].balance}</td>
+                    <td key={tableData[i].balance}><span>£</span>{tableData[i].balance}</td>
                     <td className="text-info updateBalance deposit">deposit</td>
                     <td className="text-warning updateBalance withdraw">withdraw</td>
-                    <td className="text-success updateBalance update">update</td>
                     <td className="text-danger updateBalance delete">delete</td>
                 </tr>
             )
         }
         return result;
-    }
+    };
 
     render() {
         return (
             <div>
                 <table className="table table-hover">
-                    {this.generateHeader()}
+                    <thead>
+                        <tr>
+                            {this.generateHeader()}
+                        </tr>
+                    </thead>
                     <tbody>
                         {this.generateTableData()}
                     </tbody>
