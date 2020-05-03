5a12e07fe03238b04121aa8801a674634a612182
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-05-03 14:07:34 +0100

    displaying account balance successfully, basic input validation and error message showing, successfully grabing input

--- bankingAppFE/banking-app/src/Components/CustomerAccountModal/CustomerAccountModal.js
+++ bankingAppFE/banking-app/src/Components/CustomerAccountModal/CustomerAccountModal.js
@@ -6,22 +6,71 @@ class CustomerAccountModal extends React.Component{
         super(props);
 
         this.state = {
-
+            response: ''
         }
     }
 
     displayName = () => {
-        let result = []
-        let accountInfo = this.props.accountInfo
-        result.push(<p>account holder name: {accountInfo.fname} {accountInfo.sname}</p>)
+        let result = [];
+        let accountInfo = this.props.accountInfo;
+        result.push(<h3 key="acc-name">{accountInfo.fname} {accountInfo.sname}</h3>);
         return result
-    }
+    };
+
+    displayBalance = () => {
+        let result = [];
+        result.push(
+            <div>
+                <p key="acc-balance-title">Account balance:</p>
+                <h3 key="acc-balance"><span>£</span>{this.props.accountInfo.balance}</h3>
+            </div>
+        );
+        return result
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let depositReq = e.target[0].checked;
+        let withdrawReq = e.target[1].checked;
+        let amountReq = e.target[2].value;
+
+        if (depositReq === false && withdrawReq === false) {
+            this.updateResponse('Please select either the deposit or request option.')
+        } else if (amountReq === '') {
+            this.updateResponse('Please specify how much you would like to deposit or withdraw.')
+        }
+
+        
+
+    };
+
+    updateResponse = (newResponse) => {
+        setTimeout(()=> {
+            this.clearResponse()
+        }, 3000);
+        this.setState({response : newResponse})
+    };
+
+    clearResponse = () => {
+        this.setState({response : ''})
+    };
 
     render() {
         return (
             <div className="camContainer">
-                ey yo!
                 {this.displayName()}
+                {this.displayBalance()}
+                <form onSubmit={this.handleSubmit}>
+                    <input type="radio" id="depositRadio" name="updateBalance" value="depositRadio"/>
+                        <label htmlFor="depositRadio" className="text-info">Deposit</label>
+                    <input type="radio" id="withdrawRadio" name="updateBalance" value="withdrawRadio"/>
+                        <label htmlFor="withdrawRadio" className="text-warning">Withdraw</label>
+                    <input className="modalInputNum" type="number"/>
+                    <button type="submit" className="btn btn-success">Submit</button>
+                </form>
+                <button className="btn btn-danger">Delete Account</button>
+                <button className="btn btn-dark">Cancel Update</button>
+                <div className="text-danger">{this.state.response}</div>
             </div>
         );
     }
